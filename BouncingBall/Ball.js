12b421d704a96efc97c8de13bf09185e28852338
seolin <iseolin76@gmail.com>
2020-11-25 23:40:34 +0900

    파일을 나눔

--- BouncingBall/Ball.js
+++ BouncingBall/Ball.js
@@ -1,4 +1,5 @@
 export class Ball {
+  //Ball 생성자
   constructor(stageWidth, stageHeight, radius, speed) {
     this.radius = radius;
     this.vx = speed;
@@ -9,11 +10,15 @@ export class Ball {
     this.y = diameter + (Math.random() * stageHeight - diameter);
   }
 
-  draw(ctx, stageWidth, stageHeight) {
+  draw(ctx, stageWidth, stageHeight, block) {
     this.x += this.vx;
     this.y += this.vy;
 
+    //화면에 닿으면 튕기는 함수 호출
     this.bounceWindow(stageWidth, stageHeight);
+
+    //Block에 닿으면 튕기는 함수 호출
+    this.bounceBlock(block);
 
     ctx.fillStyle = '#fdd700';
     ctx.beginPath();
@@ -21,6 +26,7 @@ export class Ball {
     ctx.fill();
   }
 
+  //화면에 닿으면 튕기는 함수
   bounceWindow(stageWidth, stageHeight) {
     const minX = this.radius;
     const maxX = stageWidth - this.radius;
@@ -30,9 +36,35 @@ export class Ball {
     if (this.x <= minX || this.x >= maxX) {
       this.vx *= -1;
       this.x += this.vx;
-    } else if (this.y <= minY || this.Y >= maxY) {
+    } else if (this.y <= minY || this.y >= maxY) {
       this.vy *= -1;
       this.y += this.vy;
+    }
+  }
+
+  //Block에 닿으면 튕기는 함수
+  bounceBlock(block) {
+    const minX = block.x - this.radius;
+    const maxX = block.maxX + this.radius;
+    const minY = block.y - this.radius;
+    const maxY = block.maxY + this.radius;
+
+    if (this.x > minX && this.x < maxX && this.y > minY && this.y < maxY) {
+      const x1 = Math.abs(minX - this.x);
+      const x2 = Math.abs(this.x - maxX);
+      const y1 = Math.abs(minY - this.y);
+      const y2 = Math.abs(this.y - maxY);
+      const min1 = Math.min(x1, x2);
+      const min2 = Math.min(y1, y2);
+      const min = Math.min(min1, min2);
+
+      if (min == min1) {
+        this.vx *= -1;
+        this.x += this.vx;
+      } else if (min == min2) {
+        this.vy *= -1;
+        this.y += this.vy;
+      }
     }
   }
 }
